496c5f5e8307b14b4a25a30fd7452bb3fd4893a6
Lương <trinhhoangluong01@gmail.com>
2019-12-13 16:21:44 +0700

    create project add model controller view for required 1

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace _1611061593_lab2
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "list categories",
+                url: "list-categories",
+                defaults: new { controller = "Categories", action = "Index"}
+            );
+            routes.MapRoute(
+                name: "list news",
+                url: "list-news",
+                defaults: new { controller = "News", action = "Index"}
+            );
+            routes.MapRoute(
+                name: "category details get by id",
+                url: "category-details-{id}",
+                defaults: new { controller = "Categories", action = "Details", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "News details",
+                url: "news-details-{id}",
+                defaults: new { controller = "News", action = "Details", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Default",
+                url: "",
+                defaults: new { controller = "Home", action = "Index"}
+            );
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1611061593_lab2.MvcApplication" Language="C#" %>
